3183b3cddc85826806cfbf503ae77f85c3df27fc
isaac ceresa da silva <114491508+isaacwrst@users.noreply.github.com>
2024-05-15 08:03:54 -0300

    dfg

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const textos = document.querySelectorAll(".aba-conteudo");
 for(let i = 0; i < botoes,length; i++) {
     botoes[i].onclick = function () {
 
-        for (let i = 0; i < botoes.length; i++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
 
+        for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
